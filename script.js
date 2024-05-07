a668d7bd5ff1c7db84566f57dabd6264bc8ab4a4
Alyona <alyona.evg@yandex.ru>
2024-05-07 18:37:19 +0300

    added hero swiper and nav-socials

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 'use strict';
 
-// const swiper = new Swiper('.hero-swiper', {
-//     speed: 400,
-//     spaceBetween: 100,
-//     navigation: {
-//         enabled: true,
-//         nextEl: 'swiper-button-next',
-//         prevEl: 'swiper-button-prev',
-//     }
-//   });
+const swiper = new Swiper('.hero-swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    direction: 'horizontal',
+    allowTouchMove: true,
+    navigation: {
+        enabled: true,
+        nextEl: '.hero-button-next',
+        prevEl: '.hero-button-prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
 
   const swiperAnnouncement = new Swiper('.swiper-announcement', {
       // Optional parameters
       direction: 'horizontal',
-      loop: true,
-      slidesPerView: "auto",
+      slidesPerView: 3,
+      spaceBetween: 20,
     
       // Navigation arrows
       navigation: {
         enabled: true,
-        nextEl: 'swiper-button-next',
-        prevEl: 'swiper-button-prev'
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
       }
     });  
