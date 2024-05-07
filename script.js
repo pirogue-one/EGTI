aa4c01b3787ee714ff20256cd7c5d8eee09bd9ee
aleriiu <aleriiiu@gmail.com>
2024-05-07 14:44:20 +0300

    added html/css

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 'use strict';
 
-const swiper = new Swiper('.hero-swiper', {
-    speed: 400,
-    spaceBetween: 100,
-    navigation: {
+// const swiper = new Swiper('.hero-swiper', {
+//     speed: 400,
+//     spaceBetween: 100,
+//     navigation: {
+//         enabled: true,
+//         nextEl: 'swiper-button-next',
+//         prevEl: 'swiper-button-prev',
+//     }
+//   });
+
+  const swiperAnnouncement = new Swiper('.swiper-announcement', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: "auto",
+    
+      // Navigation arrows
+      navigation: {
         enabled: true,
         nextEl: 'swiper-button-next',
-        prevEl: 'swiper-button-prev',
-    }
-  });
+        prevEl: 'swiper-button-prev'
+      }
+    });  
